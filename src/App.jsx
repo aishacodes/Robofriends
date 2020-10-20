a1605fde55cbd9b161bcfaa631a5f64c3fad0ab5
aishacodes <aishatabdulfatah81@gmail.com>
2020-10-20 23:44:33 +0100

    feat: code cleanup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+ const App = () =>{
+   return(
+     <div className="bg-white">
+        <p className="text-white-600" >Robofriends</p>
+      </div>
+   )
+ }
+
+export default App;
