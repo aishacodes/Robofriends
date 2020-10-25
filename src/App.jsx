b54234b20e298c7fead97bf102b8885714a1405a
aishacodes <aishatabdulfatah81@gmail.com>
2020-10-25 23:50:09 +0100

    fix: header and search style

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import './App.css';
 
  const App = () =>{
    return(
-     <div className="py-12 px-auto w-screen	">
+     <div className="w-screen flex flex-col items-center my-6	">
         <h1 className="text-white-600 text-4xl" >Codevillagefriends</h1>
-        <input type="text" className="px-2 h-8 " placeholder="search" />
+        <input class="shadow appearance-none border rounded w-6/12 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline my-5" type="text" placeholder="Search for your friends" />
       </div>
    )
  }
